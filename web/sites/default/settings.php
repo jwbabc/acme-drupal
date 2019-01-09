803dd011a3f6709c67473e85e32ee84ccb91ca24
Joel Back <joel.back@sportngin.com>
2019-01-08 21:49:07 -0600

    Removed hash salt from settings.php, now placed in local file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -232,25 +232,6 @@ $config_directories = [];
  */
 
 /**
- * Salt for one-time login links, cancel links, form tokens, etc.
- *
- * This variable will be set to a random value by the installer. All one-time
- * login links will be invalidated if the value is changed. Note that if your
- * site is deployed on a cluster of web servers, you must ensure that this
- * variable has the same value on each server.
- *
- * For enhanced security, you may set this variable to the contents of a file
- * outside your document root; you should also ensure that this file is not
- * stored with backups of your database.
- *
- * Example:
- * @code
- *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
- * @endcode
- */
-$settings['hash_salt'] = '8Wqx3NrJwilCKnyJWb9jda1JV2xHT-qgCB0dclgjpZN6sJfNH8zhe27YDZLt-aGAjNwMp6tg9g';
-
-/**
  * Deployment identifier.
  *
  * Drupal's dependency injection container will be automatically invalidated and
